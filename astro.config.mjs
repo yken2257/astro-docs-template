635a1a4f543e6f1f17f0a8571b604d6a6950323e
yken2257 <kento-yoshida@kke.co.jp>
2023-10-05 23:56:08 +0000

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import rehypeSlug from 'rehype-slug';
+import rehypeExternalLinks from 'rehype-external-links';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [tailwind()],
+  markdown: {
+		remarkPlugins: [],
+		rehypePlugins: [
+			rehypeSlug,
+			[rehypeExternalLinks, {rel: ['noopener', 'noreferrer'], target: '_blank'}]
+		],
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'material-theme',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#333',
+            a: {
+              color: '#3182ce',
+              '&:hover': {
+                color: '#2c5282',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+	plugins: [require('@tailwindcss/typography'),],
+};
